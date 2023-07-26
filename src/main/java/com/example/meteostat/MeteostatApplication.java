5c8bdafd62f03aee296444122ab460db444570d7
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-07-26 11:30:45 -0600

    Initial commit

---
+++ src/main/java/com/example/meteostat/MeteostatApplication.java
@@ -0,0 +1,13 @@
+package com.example.meteostat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteostatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteostatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meteostat/MeteostatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meteostat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteostatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
